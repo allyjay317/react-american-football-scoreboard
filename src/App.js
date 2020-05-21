9406eecf64f54176f7955980bb6f0f4920fea8df
Allison Usher <allyjay317@gmail.com>
2020-05-20 19:59:30 -0700

    did work on stretch goal 1

--- src/App.js
+++ src/App.js
@@ -1,52 +1,86 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, { useState } from "react";
+import React, { useState} from "react";
 import BottomRow from "./BottomRow";
 import "./App.css";
+import { useEffect } from "react";
+
+
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-  const [homeScore, setHomeScore] = useState(0)
-  const [awayScore, setAwayScore] = useState(0)
+  const homeTeam = new Team("Lions", "home")
+  const awayTeam = new Team("Tigers", "away")
+  const [seconds, setSeconds] = useState(10)
+  const [minutes, setMinutes] = useState(0)
+  const [isActive, setIsActive] = useState(true)
+  useEffect(() =>{
+    let interval = null
+    if(isActive){
+    interval = setInterval(() => {
+      if(seconds <= 0){
+        if(minutes<= 0){
+          setIsActive(false);
+          return () => clearInterval(interval)
+        }
+        setMinutes(minutes => minutes-1)
+        setSeconds(59)
+      }
+      else{
+        setSeconds(seconds => seconds-1)
+      }
+    }, 1000)
+  }
+    return () => clearInterval(interval)
+  }, [minutes, seconds, isActive])
+  
   return (
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
-
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-            <div className="home__score">{homeScore}</div>
-          </div>
-          <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{awayScore}</div>
-          </div>
+          <TeamDisplay team={homeTeam} />
+  <div className="timer">{minutes<10 ? "0"+minutes : minutes}:{seconds<10 ? "0"+seconds : seconds}</div>
+          <TeamDisplay team={awayTeam} />
         </div>
         <BottomRow />
       </section>
       <section className="buttons">
-        <div className="homeButtons">
-
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={() => setHomeScore(homeScore+7)}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick={() => setHomeScore(homeScore+3)}>Home Field Goal</button>
-        </div>
-        <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={() => setAwayScore(awayScore+7)}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={() => setAwayScore(awayScore+3)}>Away Field Goal</button>
-        </div>
+        <ScoreButton team={homeTeam} />
+        <ScoreButton team={awayTeam} />
       </section>
     </div>
   );
 }
 
-function TeamDisplay(){
-  
+function TeamDisplay(props){
+  return (
+    <div className={props.team.location}>
+            <h2 className={props.team.location + "__name"}>{props.team.teamName}</h2>
+
+            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
+            <div className={props.team.location + "__score"}>{props.team.score}</div>
+          </div>
+  )
 }
 
-function ScoreButton(){
+function ScoreButton(props){
+  return (
+    <div className={props.team.location + "Buttons"}>
+      <button className={props.team.location + "Buttons__touchdown"} onClick={() => props.team.addPoints(7)}>{props.team.teamName} Touchdown</button>
+      <button className={props.team.location + "Buttons__fieldGoal"} onClick={() => props.team.addPoints(3)}>{props.team.teamName} Field Goal</button>
+    </div>
+  )
+}
 
+class Team {
+  constructor(name, location){
+    this.teamName = name;
+    this.location = location;
+    [this.score, this.setScore] = useState(0)
+    this.gameRunning = true;
+  }
+  addPoints(points){
+    this.gameRunning ? this.setScore(this.score + points) : this.setScore(this.score)
+  }
 }
 
 export default App;
