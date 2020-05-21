9f452bf9a06ad7bf5a1b916062676d5f3397d30b
Allison Usher <allyjay317@gmail.com>
2020-05-20 20:59:46 -0700

    worked on tictactoe but not completed

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import TickTacToe from './TickTacToe';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<TickTacToe />, document.getElementById('tic'))
